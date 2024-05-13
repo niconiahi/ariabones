34018feb80f29e474df0cd30aeba7b2c494a245b
Nicolas Accetta <nicolas.accetta@gmail.com>
2024-05-12 21:39:14 -0300

    chore: use vite starter to create playground

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,3 +1,16 @@
 import { registerButton } from "@ariabones/button"
 
+document.querySelector<HTMLDivElement>('#root')!.innerHTML = `
+  <section style="display: grid; grid-template-columns: 1fr; row-gap: 8px">
+    <h3>Make sure you have the Elements tab open so that you can see the changing HTML attributes</h3>
+    <button is="ariabones-button" _="on click set @disabled to true">
+      Disable button
+    </button>
+    <button is="ariabones-button" data-type="toggle" data-pressed="true">
+      Toggle button
+    </button>
+  </section>
+`
+
 registerButton()
+
